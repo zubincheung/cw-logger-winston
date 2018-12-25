6c10121a9963b37da371acf40d0400013310f0ef
Zubin <zubincheung@outlook.com>
2018-12-25 16:18:32 +0800

    update: index

--- index.js
+++ index.js
@@ -1,11 +1,2 @@
 module.exports.CWLogger = require('./lib/cw-logger');
 module.exports.levels = require('./lib/level');
-
-const log = new module.exports.CWLogger({
-  consoleFormatter: meta => {
-    return `${meta.hostname} ${meta.pid} ${meta.level} (${meta.ms}) [${meta.module}] ${
-      meta.message
-    }`;
-  },
-});
-log.error(new Error('23332'));
